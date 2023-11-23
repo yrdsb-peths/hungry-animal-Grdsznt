8ee4f0c458c7e832280647ea7fe9b991b6e42ca5
Grdsznt <edwin7x7@gmail.com>
2023-11-23 09:23:48 -0500

    make changes to dev branch

--- Elephant.java
+++ Elephant.java
@@ -8,16 +8,42 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Elephant extends Actor
 {
-    /**
-     * Act - do whatever the Elephant wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+    GreenfootSound elephantSound = new GreenfootSound("elephantcub.mp3");
+    GreenfootImage[] idleRight = new GreenfootImage[8];
+    GreenfootImage[] idleLeft = new GreenfootImage[8];
+    SimpleTimer aniTimer = new SimpleTimer();
+    String facing = "right";
+    
+    public Elephant() {
+        for (int i = 0;i<8;i++) {
+            idleRight[i] = new GreenfootImage("images/elephant_idle/idle" + i + ".png"); 
+            idleRight[i].scale(75, 75);
+        }
+        for (int i = 0;i<8;i++) {
+            idleLeft[i] = new GreenfootImage("images/elephant_idle/idle" + i + ".png"); 
+            idleLeft[i].mirrorHorizontally();
+            idleLeft[i].scale(75, 75);
+        }
+        setImage(idleRight[0]);
+        
+        aniTimer.mark();
+    }
+    int imageIndex = 0;
+    public void animate() {
+        if (aniTimer.millisElapsed() < 100) return;
+        aniTimer.mark();
+        if(facing.equals("right")) {
+            setImage(idleRight[imageIndex]);
+        } else setImage(idleLeft[imageIndex]);
+
+        imageIndex = (imageIndex + 1) % 8;
+    }
     public void act()
     {
         if(Greenfoot.isKeyDown("left")) {
-            move(-4);
+            move(-4); facing = "left";
         } else if (Greenfoot.isKeyDown("right")) {
-            move(4);
+            move(4); facing = "right";
         } 
         
         if(isTouching(Apple.class)){
@@ -25,6 +51,9 @@ public class Elephant extends Actor
             MyWorld world = (MyWorld) getWorld();
             world.increaseScore();
             world.spawnApple();
+            elephantSound.play();
         }
+        
+        animate();
     }
 }
